d76f7469d4f71fc1dfc38069a98c127c2535f6e3
jishilei <jishilei@baidu.com>
2017-05-17 14:51:11 +0800

    code style
    Change-Id: I4d85da4051f49122b89886ecd203fd191f5bfd53

--- studio-api/src/main/java/com/baidu/hugegraph/studio/execution/gremlin/GremlinExecutor.java
+++ studio-api/src/main/java/com/baidu/hugegraph/studio/execution/gremlin/GremlinExecutor.java
@@ -1,5 +1,6 @@
 package com.baidu.hugegraph.studio.execution.gremlin;
 
+import com.baidu.hugegraph.studio.notebook.model.NotebookCell;
 import com.baidu.hugegraph.studio.notebook.model.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +11,7 @@ public class GremlinExecutor {
     private static final Logger logger = LoggerFactory.getLogger(GremlinExecutor.class);
     private static final int RESULT_SIZE_LIMIT = 1000;
 
-    public Result execute(String notebookId, String code, String executionEngine) throws Exception {
+    public Result execute(String notebookId, String code, NotebookCell.GremlinExecutionEngine executionEngine) throws Exception {
         return null;
     }
 }
