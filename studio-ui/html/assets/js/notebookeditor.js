757756ad2b5e9fcc0542972b1601c32c2cceb143
liunanke <liunanke@baidu.com>
2017-05-22 16:38:40 +0800

    HugeGraph-132 : Remove printMargin and modified the editor's display
    Change-Id: I3460ef37d8b51af921b96c8ed5c99b873ae99c6b

--- studio-ui/html/assets/js/notebookeditor.js
+++ studio-ui/html/assets/js/notebookeditor.js
@@ -1,12 +1,14 @@
 /**
  * Created by liunanke on 2017/5/22.
  */
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/chrome");
-editor.session.setMode("ace/mode/gremlin");
-ace.require("ace/ext/language_tools");
-editor.getSession().setTabSize(4);
-
+var editor = ace.edit('editor');
+ace.require('ace/ext/old_ie');
+ace.require('ace/ext/language_tools');
+editor.setTheme('ace/theme/chrome');
+editor.session.setMode('ace/mode/gremlin');
+editor.setShowPrintMargin(false);
+document.getElementById('editor').style.fontSize = '16px';
+editor.resize();
 editor.setOptions({
     enableBasicAutocompletion: true,
     enableSnippets: true,
