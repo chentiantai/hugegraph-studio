7a4e284b615d193cb2aa0cc17c53c4dafc5a7107
huanghaiping02 <huanghaiping02@baidu.com>
2017-05-22 16:02:48 +0800

    HugeGraph-129 : hugegrap-studio-ui 中card.html
    Change-Id: Ibd72adba22c37939135952c79d293914b238d4b5

---
+++ studio-ui/html/assets/js/graph.js
@@ -0,0 +1,98 @@
+/**
+ * @file Created by huanghaiping02 on 17/5/9.
+ */
+
+
+
+$(function () {
+    var onLoad = function onLoad() {
+
+        $.getJSON('/assets/data/schema.json', function (result) {
+            if (result.status === 200) {
+
+                var schema = result.data;
+                drawGraph(schema);
+            } else {
+
+                alert(result.message);
+            }
+        });
+    };
+
+    onLoad();
+
+
+    function drawGraph(schema) {
+
+        var nodes = new vis.DataSet();
+        nodes.on('*', function () {
+            //  do something
+        });
+
+        schema.vertexLabels.map(function (item) {
+            nodes.add([
+                {id: item.name, label: item.name, title: item.name}
+            ]);
+        });
+
+        var edges = new vis.DataSet();
+        edges.on('*', function () {
+            // do something
+        });
+        var i = 0;
+        schema.edgeLabels.map(function (item) {
+            item.connections.map(function (conn) {
+                i = i + 1;
+                edges.add([
+                    {id: i, from: conn.srcVertex, to: conn.tgtVertex, label: item.name},
+                ]);
+            });
+
+        });
+
+        var container = document.getElementById('graph');
+        var data = {
+            nodes: nodes,
+            edges: edges
+        };
+        var options = {
+            interaction: {hover: true},
+            physics: true,
+            nodes: {
+                shape: 'circle',
+                size: 20,
+                font: {
+                    size: 8
+                },
+                widthConstraint: {
+                    maximum: 80
+                },
+                heightConstraint: {valign: 'middle'},
+                shadow: true,
+                color: {
+                    background: '#b9baba', border: '#b9baba',
+                    highlight: {background: '#0fa2f6', border: '#0fa2f6'},
+                    hover: {background: '#06e4f8', border: '#06e4f8'}
+                }
+            },
+            edges: {
+                shadow: true,
+                smooth: {
+                    type: 'dynamic'
+                },
+                font: {
+                    size: 8
+                },
+                arrows: 'to',
+                color: {highlight: '#0fa2f6', hover: '#06e4f8'},
+                length: 150
+
+            }
+        };
+        var network = new vis.Network(container, data, options);
+        network.on('showPopup', function (params) {
+            document.getElementById('eventSpan').innerHTML = '<h2>showPopup event: </h2>' + JSON.stringify(params, null, 4);
+        });
+    }
+});
+
