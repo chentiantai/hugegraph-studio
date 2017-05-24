44b9dcbd80e27e6ae87bf6ac3e3c1c29224df29b
liunanke <liunanke@baidu.com>
2017-05-24 14:29:18 +0800

    HugeGraph-142: add Markdown highlightRule && dynamically changing the height of the ace-editor
    Change-Id: I0287adfc88e20393188326cd5aaa564b121ab40d

--- studio-ui/html/assets/js/notebookeditor.js
+++ studio-ui/html/assets/js/notebookeditor.js
@@ -7,6 +7,9 @@ ace.require('ace/ext/language_tools');
 editor.setTheme('ace/theme/chrome');
 editor.session.setMode('ace/mode/gremlin');
 editor.setShowPrintMargin(false);
+editor.setAutoScrollEditorIntoView(true);
+editor.setOption('maxLines', 30);
+editor.setOption('minLines', 2);
 document.getElementById('editor').style.fontSize = '16px';
 editor.resize();
 editor.setOptions({
@@ -14,3 +17,20 @@ editor.setOptions({
     enableSnippets: true,
     enableLiveAutocompletion: true
 })
+
+var editorM = ace.edit('editorM');
+ace.require('ace/ext/old_ie');
+ace.require('ace/ext/language_tools');
+editorM.setTheme('ace/theme/chrome');
+editorM.session.setMode('ace/mode/markdown');
+editorM.setShowPrintMargin(false);
+editorM.setAutoScrollEditorIntoView(true);
+editorM.setOption('maxLines', 30);
+editorM.setOption('minLines', 2);
+document.getElementById('editorM').style.fontSize = '16px';
+editorM.resize();
+editorM.setOptions({
+    enableBasicAutocompletion: true,
+    enableSnippets: true,
+    enableLiveAutocompletion: true
+});
