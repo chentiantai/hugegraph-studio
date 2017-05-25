c6b26fdf63b846a7ce05e886cd160bec67246cde
jishilei <jishilei@baidu.com>
2017-05-25 15:49:58 +0800

    HugeGraph-153 , getConnectionSchema
    Change-Id: I28fabe87b644d5362994a3dba037f09caeaf93fc

--- studio-api/src/main/java/com/baidu/hugegraph/studio/connections/service/ConnectionService.java
+++ studio-api/src/main/java/com/baidu/hugegraph/studio/connections/service/ConnectionService.java
@@ -3,7 +3,6 @@ package com.baidu.hugegraph.studio.connections.service;
 import com.baidu.hugegraph.studio.connections.model.Connection;
 import com.baidu.hugegraph.studio.connections.model.ConnectionState;
 import com.baidu.hugegraph.studio.connections.repository.ConnectionRepository;
-import com.baidu.hugegraph.studio.notebook.model.Notebook;
 import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -86,6 +85,21 @@ public class ConnectionService {
         // TODO : connect HugeServer to get connection status
         Response response = Response.status(200)
                 .entity(ConnectionState.CLOSED)
+                .build();
+        return response;
+    }
+
+    @GET
+    @Path("schema")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getConnectionSchema(@PathParam("connectionId") String connectionId){
+        Preconditions.checkNotNull(connectionId);
+
+        // TODO : check connection status first
+
+        // TODO : use hugegraph-client , return get schema ;
+
+        Response response = Response.status(200)
                 .build();
         return response;
     }
