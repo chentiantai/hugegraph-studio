2dde27c382f84e6e503e74c7cc106c0d33caafa1
jishilei <jishilei@baidu.com>
2017-05-26 14:15:31 +0800

    HugeGraph-154
    Change-Id: Ie859dc9a97c19f2400d5acfcaf86ad5a7b074c50

--- studio-api/src/main/java/com/baidu/hugegraph/studio/connections/service/ConnectionService.java
+++ studio-api/src/main/java/com/baidu/hugegraph/studio/connections/service/ConnectionService.java
@@ -12,16 +12,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-//import javax.ws.rs.Path;
-//import javax.ws.rs.POST;
-//import javax.ws.rs.DELETE;
-//import javax.ws.rs.GET;
-//import javax.ws.rs.PUT;
-//import javax.ws.rs.PathParam;
-//import javax.ws.rs.Produces;
-//import javax.ws.rs.Consumes;
-//import javax.ws.rs.core.MediaType;
-//import javax.ws.rs.core.Response;
+// import javax.ws.rs.Path;
+// import javax.ws.rs.POST;
+// import javax.ws.rs.DELETE;
+// import javax.ws.rs.GET;
+// import javax.ws.rs.PUT;
+// import javax.ws.rs.PathParam;
+// import javax.ws.rs.Produces;
+// import javax.ws.rs.Consumes;
+// import javax.ws.rs.core.MediaType;
+// import javax.ws.rs.core.Response;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.POST;
@@ -34,9 +34,9 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-//import java.util.List;
-//import java.util.Map;
-//import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
+// import java.util.HashMap;
 
 import java.util.List;
 import java.util.Map;
