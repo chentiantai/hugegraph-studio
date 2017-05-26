d0f23807dd12b69efb8608afdc433380b16c94e2
jishilei <jishilei@baidu.com>
2017-05-26 14:13:55 +0800

    HugeGraph-154
    Change-Id: I3c8e0cfe68e1c8a5b65df1c42ee86f3e345af11f

--- studio-api/src/main/java/com/baidu/hugegraph/studio/connections/service/ConnectionService.java
+++ studio-api/src/main/java/com/baidu/hugegraph/studio/connections/service/ConnectionService.java
@@ -12,10 +12,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.ws.rs.*;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.POST;
+//import javax.ws.rs.DELETE;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.PUT;
+//import javax.ws.rs.PathParam;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.Consumes;
+//import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.Response;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.*;
+
+//import java.util.List;
+//import java.util.Map;
+//import java.util.HashMap;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Created by jishilei on 2017/5/19.
