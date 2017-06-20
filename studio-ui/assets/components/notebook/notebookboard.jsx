7b73f65b1dc513c00d623cafa6931a162d93bd34
huanghaiping02 <huanghaiping02@baidu.com>
2017-06-20 15:04:02 +0800

    HugeGraph-296 : add item of notebook
    Change-Id: I54b5da1755552d3e349494af93e0379376ab6db6

--- studio-ui/assets/components/notebook/notebookboard.jsx
+++ studio-ui/assets/components/notebook/notebookboard.jsx
@@ -11,43 +11,64 @@ import {itemScreenMode} from './actions';
 class NotebookBoard extends React.Component {
     constructor() {
         super();
-        this.itemKey = [0,1,2];
+        this.state = {
+            itemKeys: [0]
+        }
     }
 
 
     render() {
+        console.log(this.state.itemKeys);
         let fullScreenItem = this.props.screenMode.itemKey;
         let fullScreen = this.props.screenMode.fullScreen;
-        let display = fullScreen ? 'none' : 'block';
-        let content = this.itemKey.map(i =>
-            <div key={i} >
-                <NotebookItem itemKey={i}
-                              display={fullScreen ? fullScreenItem === i ? 'block' : 'none' : 'block'}/>
-                <NoteBookItemAdd itemKey={i + '_add'} display={display}/>
-            </div>
-        );
-
-
-
+        let itemDisplay = fullScreen ? fullScreenItem === i ? 'block' : 'none' : 'block'
+        let addDisplay = fullScreen ? 'none' : 'block';
         return (
             <div>
-                {content}
+                {
+                    this.state.itemKeys.map(i =>
+                        <div key={i}>
+                            <NotebookItem itemKey={i}
+                                          display={itemDisplay}/>
+                            <NoteBookItemAdd itemKey={i} display={addDisplay} onClick={this.addItem}/>
+                        </div>)
+                }
             </div>
         );
+    }
+
+    addItem = (itemKey) => {
+        let newItemKeys = [];
+        this.state.itemKeys.forEach(i => {
+                if (i === itemKey) {
+                    newItemKeys.push(i);
+                    newItemKeys.push(this.state.itemKeys.length);
+                } else {
+                    newItemKeys.push(i);
+                }
+            }
+        );
+        this.setState({
+            itemKeys: newItemKeys
+        });
     }
 }
 
 
 class NoteBookItemAdd extends React.Component {
+    onClick = () => {
+        this.props.onClick(this.props.itemKey);
+    }
+
     render() {
         return (
             <div className="container" style={{display: this.props.display}}>
-                <div className="row card">
+                <div className="row">
                     <div className="col-md-12 col-box card-add">
                         <div className="card-add-line"></div>
                         <div className="card-add-btn">
                             <div className="btn-group btn-group-sm" role="group">
-                                <button type="button" className="btn btn-default">
+                                <button type="button" className="btn btn-default" onClick={this.onClick}>
                                     <i className="fa fa-plus" aria-hidden="true"></i>
                                 </button>
                             </div>
