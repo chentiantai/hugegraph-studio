41fc8e8b631cc668a88df402a2ad0871d9a7370e
huanghaiping02 <huanghaiping02@baidu.com>
2017-06-23 10:56:46 +0800

    HugeGraph-323 :  the layout of notecard div
    Change-Id: I3db70d5ce8566e2ed8c60802e4b4f2239cc856c8

--- studio-ui/assets/components/notebooksApp/notecard.jsx
+++ studio-ui/assets/components/notebooksApp/notecard.jsx
@@ -20,22 +20,20 @@ class NoteCard extends React.Component {
         let notebookUrl = 'notebook.html?id=' + noteCard.id + '&name=' + noteCardName;
         return (
             <div className="notebook-card">
+                <div className="notebook-card-close">
+                    <button type="button" className="btn btn-link"
+                            onClick={() => this.del()}>
+                        <i className="fa fa-times"
+                           aria-hidden="true"></i>
+                    </button>
+                </div>
+                <a href={notebookUrl}>
                 <div className="notebook-card-info">
-                    <div>
-                        <div className="notebook-card-close">
-                            <button type="button" className="btn btn-link"
-                                    onClick={() => this.del()}>
-                                <i className="fa fa-times"
-                                   aria-hidden="true"></i>
-                            </button>
-                        </div>
-                        <div className="notebook-card-title">
-                            <a href={notebookUrl}>
-                                {
-                                    noteCardName
-                                }
-                            </a>
-                        </div>
+                    <div className="notebook-card-title">
+                            {
+                                noteCardName
+                            }
+
                     </div>
                     <div className="notebook-card-description">
                         {
@@ -43,6 +41,7 @@ class NoteCard extends React.Component {
                         }
                     </div>
                 </div>
+                </a>
                 <div className="notebook-card-control">
 
                     <div className="notebook-card-createtime">
