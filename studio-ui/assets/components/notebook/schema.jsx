e62745068095563f9f04e5b471a098b799edc87e
liunanke <liunanke@baidu.com>
2017-08-09 14:10:07 +0800

    HugeGraph-580:Add Schema-view Tooltips for HugeStudio
    Change-Id: I15cc4cd11efe0478751da85d5cd75f3a3d0fe082

--- studio-ui/assets/components/notebook/schema.jsx
+++ studio-ui/assets/components/notebook/schema.jsx
@@ -3,9 +3,11 @@
  * @author liunanke(liunanke@baidu.com)
  * Created on 2017/6/1.
  */
-import React from "react";
-import {showSchema} from "./actions";
-import {connect} from "react-redux";
+import React from 'react';
+import {showSchema} from './actions';
+import {connect} from 'react-redux';
+import {OverlayTrigger, ButtonToolbar, Popover} from 'react-bootstrap';
+
 
 export class Schema extends React.Component {
     constructor() {
@@ -19,41 +21,182 @@ export class Schema extends React.Component {
                 <div>
                     <div className="container schema-container">
                         <div className="row schema">
-                            <h5>Node
-                                labels({schemaResult.vertexLabels.length})</h5>
+                            <h5>Vertex
+                                Labels({schemaResult.vertexLabels.length})</h5>
                             <div className="schema-box">
-                                {
-                                    schemaResult.vertexLabels.map(
-                                        (vertexLabel, index) =>
-                                            <span key={index}
-                                                  className="label label-default schema-span"> {vertexLabel.name}</span>
-                                    )
-                                }
+                                <ButtonToolbar>
+                                    {
+                                        schemaResult.vertexLabels.map(
+                                            (vertexLabel, index) => {
+                                                let tooltip = <Popover
+                                                    id="schema-tooltip"
+                                                    title="VertexLabel">
+                                                    {
+                                                        <div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">Name</span>:
+                                                                <span
+                                                                    className="property_value_span">{vertexLabel.name}</span>
+                                                            </div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">Properties</span>:
+                                                                <span
+                                                                    className="property_value_span">{vertexLabel.properties.length === 0 ? '[ ]' : JSON.stringify(vertexLabel.properties)}</span>
+                                                            </div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">IndexNames</span>:
+                                                                <span
+                                                                    className="property_value_span">{vertexLabel.indexNames.length === 0 ? '[ ]' : JSON.stringify(vertexLabel.indexNames)}</span>
+                                                            </div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">PrimaryKeys</span>:
+                                                                <span
+                                                                    className="property_value_span">{vertexLabel.primaryKeys.length === 0 ? '[ ]' : vertexLabel.primaryKeys}</span>
+                                                            </div>
+                                                        </div>
+                                                    }</Popover>;
+                                                return (
+                                                    <OverlayTrigger
+                                                        key={index}
+                                                        placement="bottom"
+                                                        overlay={tooltip}>
+                                                    <span
+                                                        className="label label-default schema-span"> {vertexLabel.name}</span>
+                                                    </OverlayTrigger>);
+                                            }
+                                        )
+                                    }
+                                </ButtonToolbar>
                             </div>
                         </div>
                         <div className="row schema">
-                            <h5>Relationship
-                                types({schemaResult.edgeLabels.length})</h5>
+                            <h5>Edge
+                                Labels({schemaResult.edgeLabels.length})</h5>
                             <div className="schema-box">
-                                {
-                                    schemaResult.edgeLabels.map(
-                                        (edgeLabel, index) =>
-                                            <span key={index}
-                                                  className="label label-default schema-span"> {edgeLabel.name}</span>
-                                    )
-                                }
+                                <ButtonToolbar>
+                                    {
+                                        schemaResult.edgeLabels.map(
+                                            (edgeLabel, index) => {
+                                                let tooltip = <Popover
+                                                    id="schema-tooltip"
+                                                    title="EdgeLabels">
+                                                    {
+                                                        <div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">Name</span>:
+                                                                <span
+                                                                    className="property_value_span">{edgeLabel.name}</span>
+                                                            </div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">Properties</span>:
+                                                                <span
+                                                                    className="property_value_span">{edgeLabel.properties.length === 0 ? '[ ]' : JSON.stringify(edgeLabel.properties)}</span>
+                                                            </div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">IndexNames</span>:
+                                                                <span
+                                                                    className="property_value_span">{edgeLabel.indexNames.length === 0 ? '[ ]' : JSON.stringify(edgeLabel.indexNames)}</span>
+                                                            </div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">Frequency</span>:
+                                                                <span
+                                                                    className="property_value_span">{edgeLabel.frequency}</span>
+                                                            </div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">Source</span>:
+                                                                <span
+                                                                    className="property_value_span">{edgeLabel.links[0].source}</span>
+                                                            </div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">Target</span>:
+                                                                <span
+                                                                    className="property_value_span">{edgeLabel.links[0].target}</span>
+                                                            </div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">SortKeys</span>:
+                                                                <span
+                                                                    className="property_value_span">{edgeLabel.sortKeys.length === 0 ? '[ ]' : edgeLabel.sortKeys}</span>
+                                                            </div>
+                                                        </div>
+
+                                                    }</Popover>;
+                                                return (
+                                                    <OverlayTrigger
+                                                        key={index}
+                                                        placement="bottom"
+                                                        overlay={tooltip}>
+                                                    <span
+                                                        className="label label-default schema-span"> {edgeLabel.name}</span>
+                                                    </OverlayTrigger>
+                                                );
+                                            }
+                                        )
+                                    }
+                                </ButtonToolbar>
                             </div>
                         </div>
                         <div className="row schema">
-                            <h5>Property keys({schemaResult.propertyKeys.length})</h5>
+                            <h5>Property Keys({schemaResult.propertyKeys.length})</h5>
                             <div className="schema-box">
-                                {
-                                    schemaResult.propertyKeys.map(
-                                        (propertyKey, index) =>
-                                            <span key={index}
-                                                  className="label label-default schema-span"> {propertyKey.name}</span>
-                                    )
-                                }
+                                <ButtonToolbar>
+                                    {
+                                        schemaResult.propertyKeys.map(
+                                            (propertyKey, index) => {
+                                                let tooltip = <Popover
+                                                    id="schema-tooltip"
+                                                    title="PropertyKeys">
+                                                    {
+                                                        <div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">Name</span>:
+                                                                <span
+                                                                    className="property_value_span">{propertyKey.name}</span>
+                                                            </div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">Properties</span>:
+                                                                <span
+                                                                    className="property_value_span">{propertyKey.properties.length === 0 ? '[ ]' : JSON.stringify(propertyKey.properties)}</span>
+                                                            </div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">DataType</span>:
+                                                                <span
+                                                                    className="property_value_span">{propertyKey.dataType}</span>
+                                                            </div>
+                                                            <div>
+                                                                <span
+                                                                    className="property_name_span">Cardinality</span>:
+                                                                <span
+                                                                    className="property_value_span">{propertyKey.cardinality}</span>
+                                                            </div>
+                                                        </div>
+                                                    }</Popover>;
+                                                return (
+                                                    <OverlayTrigger
+                                                        key={index}
+                                                        placement="bottom"
+                                                        overlay={tooltip}>
+                                                    <span
+                                                        className="label label-default schema-span"> {propertyKey.name}</span>
+                                                    </OverlayTrigger>
+                                                );
+                                            }
+                                        )
+                                    }
+                                </ButtonToolbar>
                             </div>
                         </div>
                     </div>
