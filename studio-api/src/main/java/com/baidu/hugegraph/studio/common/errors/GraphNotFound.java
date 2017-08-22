7327503f4179848618e8677bcca0f392254563cd
Liu Jie <liujie23@baidu.com>
2017-08-22 13:46:33 +0800

    HugeGraph-674: Syntax clean up for GraphNotFound
    Change-Id: I43d3d209029439a242174919ee0490268d2868f6
    Signed-off-by: Liu Jie <liujie23@baidu.com>

--- studio-api/src/main/java/com/baidu/hugegraph/studio/common/errors/GraphNotFound.java
+++ studio-api/src/main/java/com/baidu/hugegraph/studio/common/errors/GraphNotFound.java
@@ -19,9 +19,8 @@
 
 package com.baidu.hugegraph.studio.common.errors;
 
+public class GraphNotFound extends StudioError {
 
-public class GraphNotFound
-        extends StudioError {
     public GraphNotFound(String graphName) {
         super(502, ErrorCodes.GraphNotFound(),
               String.format("Graph '%s' does not exist.",
